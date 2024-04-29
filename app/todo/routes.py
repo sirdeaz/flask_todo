6e7e2983573c09addccc197e7b1f1b759ebcd280
Frederik Didden <frederik.didden@gmail.com>
2024-04-30 00:04:58 +0200

    Added update todo functionality

--- app/todo/routes.py
+++ app/todo/routes.py
@@ -1,6 +1,5 @@
-from curses import flash
 from flask_login import current_user, login_required
-from flask import redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for
 
 from app.extensions import db
 from app.todo import bp
@@ -16,28 +15,64 @@ def list_todos():
 @bp.route('/create')
 @login_required
 def create():
-    return render_template('create_todo.html')
+    return render_template('create_or_update_todo.html')
+
+@bp.route('/<int:todo_id>/update')
+@login_required
+def update(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id==todo_id).first()
+    if not todo:
+        flash('Todo not found.', 'error')
+        return redirect(url_for('todos.list_todos'))
+    if not todo.user_id == current_user.id:
+        flash('You are not authorized to update this todo.', 'error')
+        return redirect(url_for('todos.list_todos'))
+    return render_template('create_or_update_todo.html', todo=todo)
 
 @bp.route('/create', methods=['POST'])
+@bp.route('/<int:todo_id>/update', methods=['POST'])
 @login_required
-def create_post():
+def create_or_update(todo_id=None):
     title = request.form.get('title')
     description = request.form.get('description')
 
+    if todo_id:
+        todo = db.session.query(Todo).filter(Todo.id==todo_id).first()
+        if not todo:
+            flash('Todo not found.', 'error')
+            return redirect(url_for('todos.list_todos'))
+        if not todo.user_id == current_user.id:
+            flash('You are not authorized to update this todo.', 'error')
+            return redirect(url_for('todos.list_todos'))
+    else:
+        todo = Todo(user_id=current_user.id)
+
     if not title or not description:
         flash('All fields are required.', 'error')
-        return redirect(url_for('todos.create'))
-    
-    todo = Todo(title=title, description=description, user_id=current_user.id)
+        return redirect(url_for('todos.create' if not todo_id else 'todos.update', todo_id=todo_id))
+
+    todo.title = title
+    todo.description = description
     db.session.add(todo)
     db.session.commit()
+
+    flash('Todo saved.', 'success')
     return redirect(url_for('todos.list_todos'))
 
 
-@bp.route('/<int:todo_id>/delete')
+@bp.route('/<int:todo_id>/delete', methods=['POST'])
 @login_required
 def delete(todo_id):
     todo = db.session.query(Todo).filter(Todo.id==todo_id).first()
+
+    if not todo:
+        flash('Todo not found.', 'error')
+        return redirect(url_for('todos.list_todos'))
+
+    if todo.user_id != current_user.id:
+        flash('You are not authorized to delete this todo.', 'error')
+        return redirect(url_for('todos.list_todos'))
+    
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for('todos.list_todos'))
--- tests/functional/test_todo_pages.py
+++ tests/functional/test_todo_pages.py
@@ -1,6 +1,8 @@
 from flask import url_for
+import pytest
 from app.models.todo import Todo
 from app.extensions import db
+from app.models.user import User
 
 def test_list_todos_succeeds_if_logged_in(client, auth, user):
     auth.login(user.email, 'password')
@@ -9,20 +11,100 @@ def test_list_todos_succeeds_if_logged_in(client, auth, user):
     assert b'My Todos' in response.data
 
 def test_list_todos_fails_if_not_logged_in(client):
-    response = client.get('/todos', follow_redirects=True)
+    response = client.get('/todos/create', follow_redirects=True)
     assert response.status_code == 200
     assert response.request.path == '/auth/login'
 
 def test_create_todo_succeeds_if_logged_in(client, auth, user):
     auth.login(user.email, 'password')
-    response = client.post('/todos', follow_redirects=True)
+    response = client.post('/todos/create', follow_redirects=True)
     assert response.status_code == 200
-    assert b'create todo' in response.data
+    assert b'Create Todo' in response.data
 
 def test_create_todo_fails_if_not_logged_in(client, user):
-    response = client.post('/todos', follow_redirects=True)
+    response = client.post('/todos/create', follow_redirects=True)
     assert response.status_code == 200
     assert response.request.path == '/auth/login'
+
+@pytest.mark.parametrize(('title', 'description', 'message'), (
+    ('', '', b'All fields are required.'),
+    ('a', '',b'All fields are required.'),
+    ('', 'a',b'All fields are required.'),))
+def test_create_todo_field_validation(client, auth, user, title, description, message):
+    auth.login(user.email, 'password')
+    response = client.post('/todos/create', data={'title': title, 'description': description}, follow_redirects=True)
+    assert response.status_code == 200
+    assert message in response.data
+
+def test_create_todo_creates_new_todo(client, auth, user):
+    auth.login(user.email, 'password')
+    response = client.post('/todos/create', data={'title': 'Test Todo', 'description': 'Test Description'}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Test Todo' in response.data
+
+
+def test_update_todo_get_fails_if_not_logged_in(client, user):
+    response = client.get('/todos/1/update', follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == '/auth/login'
+
+def test_update_todo_get_fails_if_todo_does_not_exist(client, auth, user):
+    auth.login(user.email, 'password')
+    response = client.get('/todos/1/update', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Todo not found' in response.data
+
+def test_update_todo_get_fails_if_user_is_not_owner(app, client, auth, user):
+    auth.login(user.email, 'password')
+    with app.app_context():
+        other_user = User(email='test@example2.com', first_name='Test', last_name='User', password='password')
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=other_user)
+        db.session.add(todo1)
+        db.session.commit()
+        response = client.get(f'/todos/{todo1.id}/update', follow_redirects=True)
+        assert response.status_code == 200
+        assert b'You are not authorized to update this todo' in response.data
+
+def test_update_todo_get_returns_template_and_todo_when_logged_in_and_has_todo(app, client, auth, user):
+    auth.login(user.email, 'password')
+    with app.app_context():
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=user)
+        db.session.add(todo1)
+        db.session.commit()
+        response = client.get(f'/todos/{todo1.id}/update', follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Test Todo1' in response.data
+        assert b'Test Description1' in response.data
+
+def test_update_todo_post_updates_title_and_description(app, client, auth, user):
+    auth.login(user.email, 'password')
+    with app.app_context():
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=user)
+        db.session.add(todo1)
+        db.session.commit()
+
+        response = client.post(f'/todos/{todo1.id}/update', data={'title': 'Updated Title', 'description': 'Updated Description'}, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Updated Title' in response.data
+        assert b'Updated Description' in response.data
+
+def test_update_todo_post_fails_if_todo_does_not_exist(app, client, auth, user):
+    auth.login(user.email, 'password')
+    response = client.post('/todos/1/update', data={'title': 'Updated Title', 'description': 'Updated Description'}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Todo not found' in response.data
+
+def test_update_todo_post_fails_if_user_is_not_owner(app, client, auth, user):
+    auth.login(user.email, 'password')
+    with app.app_context():
+        other_user = User(email='test@example2.com', first_name='Test', last_name='User', password='password')
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=other_user)
+        db.session.add(todo1)
+        db.session.commit()
+        
+        response = client.post(f'/todos/{todo1.id}/update', data={'title': 'Updated Title', 'description': 'Updated Description'}, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'You are not authorized to update this todo' in response.data
 
 def test_list_todos_returns_template_and_todos_when_logged_in_and_has_todos(client, app, auth, user):
     auth.login(user.email, 'password')
@@ -37,3 +119,38 @@ def test_list_todos_returns_template_and_todos_when_logged_in_and_has_todos(clie
     assert b'My Todos' in response.data
     assert b'Test Todo1' in response.data
     assert b'Test Todo2' in response.data
+
+def test_delete_todo_succeeds_if_logged_in(app, client, auth, user):
+    auth.login(user.email, 'password')
+    
+    with app.app_context():
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=user)
+        db.session.add(todo1)
+        db.session.commit()
+        response = client.post(f'/todos/{todo1.id}/delete', follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Test Todo1' not in response.data
+
+def test_delete_todo_fails_if_not_logged_in(client, user):
+    response = client.post('/todos/1/delete', follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == '/auth/login'
+
+def test_delete_if_todo_does_not_exist(app, client, auth, user):
+    auth.login(user.email, 'password')
+    response = client.post('/todos/1/delete', follow_redirects=True)
+    assert response.status_code == 200
+    assert b'Todo not found' in response.data
+
+def test_delete_if_user_is_not_owner(app, client, auth, user):
+    auth.login(user.email, 'password')
+    with app.app_context():
+        other_user = User(email='test@example2.com', first_name='Test', last_name='User', password='password')
+        todo1 = Todo(title='Test Todo1', description='Test Description1', user=other_user)
+        db.session.add(todo1)
+        db.session.commit()
+        
+        response = client.post(f'/todos/{todo1.id}/delete', follow_redirects=True)
+        assert response.status_code == 200
+        assert b'You are not authorized to delete this todo' in response.data
+
